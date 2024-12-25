2d98774e3f3d6b31b8faf09fea52ec690a9a3821
jimrudolph726 <130412104+jimrudolph726@users.noreply.github.com>
2024-12-24 22:03:26 -0600

    Update film_database.py

--- film_database.py
+++ film_database.py
@@ -56,25 +56,31 @@ films_df = get_gsheet_data()
 # Title of the app
 st.title("Streamlit Film Database")
 
-# Form to input new film
-with st.form(key="film_form"):
-    title = st.text_input("Movie Title")
-    genre = st.text_input("Genre")
-    director = st.text_input("Director")
-    year = st.number_input("Year Released", min_value=1900, max_value=2024, step=1)
-    submit_button = st.form_submit_button(label="Add Film")
+# Tabs for different sections
+tabs = ["Add Film", "Films Database"]
+selected_tab = st.selectbox("Choose a section", tabs)
 
-    # Add film to the database if the submit button is pressed
-    if submit_button:
-        new_film = pd.DataFrame({"Title": [title], "Genre": [genre], "Director": [director], "Year": [year]})
-        films_df = pd.concat([films_df, new_film], ignore_index=True)
-        update_gsheet_data(films_df)
-        st.success(f"Film '{title}' added to the database!")
+if selected_tab == "Add Film":
+    # Form to input new film
+    with st.form(key="film_form"):
+        title = st.text_input("Movie Title")
+        genre = st.text_input("Genre")
+        director = st.text_input("Director")
+        year = st.number_input("Year Released", min_value=1900, max_value=2024, step=1)
+        submit_button = st.form_submit_button(label="Add Film")
 
-# Display the films in the database
-st.subheader("Films You Have Seen")
+        # Add film to the database if the submit button is pressed
+        if submit_button:
+            new_film = pd.DataFrame({"Title": [title], "Genre": [genre], "Director": [director], "Year": [year]})
+            films_df = pd.concat([films_df, new_film], ignore_index=True)
+            update_gsheet_data(films_df)
+            st.success(f"Film '{title}' added to the database!")
 
-if len(films_df) > 0:
-    st.dataframe(films_df)
-else:
-    st.write("No films added yet.")
+elif selected_tab == "Films Database":
+    # Display the films in the database
+    st.subheader("Films You Have Seen")
+
+    if len(films_df) > 0:
+        st.dataframe(films_df)
+    else:
+        st.write("No films added yet.")
